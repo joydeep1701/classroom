bfa0a19200852f21681d302055a8caa98d1ea6c0
joydeep1701 <joydeep1701@gmail.com>
2019-08-26 16:17:54 +0530

    Footer & Navbar

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+function FooterBar() {
+  return (
+    <Layout.Footer style={{ textAlign: 'center' }}>
+      <a href="https://classroom.joydeep1701.in">Classroom App</a> 2019. 
+      Created by <a href="https://joydeep1701.in">Joydeep Mukherjee</a>
+    </Layout.Footer>
+  )
+}
+
+export default FooterBar;
