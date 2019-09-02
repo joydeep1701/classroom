7ce8af9f2726cef3cffc83bbc3c2afe137873f73
joydeep1701 <joydeep1701@gmail.com>
2019-09-02 10:19:35 +0530

    API Updates

--- client/src/utils/apiPaths.js
+++ client/src/utils/apiPaths.js
@@ -1,9 +1,9 @@
-let API_HOST = "https://api.classroom.joydeep1701.in:8080";
+let API_HOST = "https://backend.joydeep1701.in";
 
-// if (process.env.NODE_ENV === 'development') {
-//   // Use react-dev-server proxy
-//   API_HOST = "http://192.168.0.100:8080"
-// }
+if (process.env.NODE_ENV === 'development') {
+  // Use react-dev-server proxy
+  API_HOST = "http://localhost:8080"
+}
 
 export const WEBSOCKET_SERVER = API_HOST;
 export const API_ROOT = API_HOST;
